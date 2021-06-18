06beaf8e0a0f38465ce3189f6f7667e43c8a19bc
Kevin Chan <kevinykchan@gmail.com>
2021-06-18 11:41:09 +0800

    Done with ASGI setting

--- taxi/settings.py
+++ taxi/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6%i3$b-rdo27-x6#hpqdkob%e4klnuf(vqligk7up4b9*mm$%h'
+SECRET_KEY = os.getenv('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,6 +73,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'taxi.wsgi.application'
 
+ASGI_APPLICATION = 'taxi.routing.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
